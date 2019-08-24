5011ab5a5fa7b6a0c43f600db13b1880ba2f1df7
Damian Jamka <damianjamka@gmail.com>
2019-08-24 12:19:55 +0200

    i dunno know

---
+++ build/precache-manifest.50af36d6e33359d9ea1425137c26026a.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbfa32fe17d8e1125d19fb7449fec1b2",
+    "url": "/static/media/slide1.cbfa32fe.png"
+  },
+  {
+    "revision": "e410e215cb8fdd5d9417",
+    "url": "/static/css/main.a7d243a7.chunk.css"
+  },
+  {
+    "revision": "83bd4da218ffd9c66f287640e6b0abc9",
+    "url": "/static/media/5.83bd4da2.JPG"
+  },
+  {
+    "revision": "f339835614d413cd08b9",
+    "url": "/static/js/1.f3398356.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "68726117da44e9dc5efae6f6ccb4f674",
+    "url": "/static/media/logo.68726117.png"
+  },
+  {
+    "revision": "7435e287d152f2726e5c6bd130e8fe9d",
+    "url": "/static/media/home-bg.7435e287.png"
+  },
+  {
+    "revision": "157288503a10e73fc929c45ef147d737",
+    "url": "/static/media/about-bg.15728850.png"
+  },
+  {
+    "revision": "8d0eea615d68f29295189c6d24a01e11",
+    "url": "/static/media/kasai.8d0eea61.png"
+  },
+  {
+    "revision": "564f1805a69ed80b480fa6772565d369",
+    "url": "/static/media/zehnder.564f1805.png"
+  },
+  {
+    "revision": "61c8ec60198c0732243188f1bfe8c54b",
+    "url": "/static/media/fujitsu.61c8ec60.png"
+  },
+  {
+    "revision": "eb18648b8d44335dc8ef05ca8adf7a40",
+    "url": "/static/media/klimor.eb18648b.png"
+  },
+  {
+    "revision": "92056013715cdb8eb81f6ef26f6a853b",
+    "url": "/static/media/slide1.92056013.png"
+  },
+  {
+    "revision": "06d0363b3e28f841efc27d37eaa3e8f8",
+    "url": "/static/media/slide2.06d0363b.png"
+  },
+  {
+    "revision": "566a4cea76abfd26dd3fd90405ee8588",
+    "url": "/static/media/slide3.566a4cea.png"
+  },
+  {
+    "revision": "0d5ef02f36eea27e60c0de89319fc875",
+    "url": "/static/media/slide1.0d5ef02f.png"
+  },
+  {
+    "revision": "62a500edc4e20acadcb648452325f34a",
+    "url": "/static/media/slide2.62a500ed.png"
+  },
+  {
+    "revision": "a07091661b186f74e5da701ef3ffe8dc",
+    "url": "/static/media/slide3.a0709166.png"
+  },
+  {
+    "revision": "1b05f8411189af9c0dbf50de0259d569",
+    "url": "/static/media/slide4.1b05f841.png"
+  },
+  {
+    "revision": "bfbffe17d827130d2083b38a20236736",
+    "url": "/static/media/slide5.bfbffe17.png"
+  },
+  {
+    "revision": "9d1021434cd2cbbd044d59a4d3717fc3",
+    "url": "/static/media/slide6.9d102143.png"
+  },
+  {
+    "revision": "e410e215cb8fdd5d9417",
+    "url": "/static/js/main.e410e215.chunk.js"
+  },
+  {
+    "revision": "7ae02131eca89e32219128e21819baf7",
+    "url": "/static/media/slide2.7ae02131.png"
+  },
+  {
+    "revision": "3577542c44da47d290ac28a6ea7362ce",
+    "url": "/static/media/slide3.3577542c.png"
+  },
+  {
+    "revision": "2402047608b9448078ab7d0ba657589b",
+    "url": "/static/media/offer-bg.24020476.png"
+  },
+  {
+    "revision": "345f9d7c18c665d087102687e7ae3b12",
+    "url": "/static/media/arrow.345f9d7c.svg"
+  },
+  {
+    "revision": "0a4a57123ffc9175965459a2cb0f1111",
+    "url": "/static/media/tel-icon.0a4a5712.svg"
+  },
+  {
+    "revision": "7020dc532ec9a898554d3991f3a997d4",
+    "url": "/static/media/address-icon.7020dc53.svg"
+  },
+  {
+    "revision": "ad68198007e6b3bfbfcbe601463e7751",
+    "url": "/static/media/mail-icon.ad681980.svg"
+  },
+  {
+    "revision": "8635814476e33682ece2431d6a2c4be1",
+    "url": "/static/media/youtube.86358144.svg"
+  },
+  {
+    "revision": "3bdb2f7200de73b5684091ed217eebd9",
+    "url": "/static/media/pinterest.3bdb2f72.svg"
+  },
+  {
+    "revision": "7f64cc545624917ed08455840fc6fb0e",
+    "url": "/static/media/instagram.7f64cc54.svg"
+  },
+  {
+    "revision": "23ad9e1b2d649f8fdf3191c9dc9238ea",
+    "url": "/static/media/facebook.23ad9e1b.svg"
+  },
+  {
+    "revision": "7ef5112d08d0dbd884ef4a11e49da54e",
+    "url": "/static/media/dyplom.7ef5112d.png"
+  },
+  {
+    "revision": "84984832c83a128f798186800df82d2b",
+    "url": "/static/media/herz.84984832.png"
+  },
+  {
+    "revision": "02f9d7826c166f21543a1be523e657d1",
+    "url": "/static/media/venture.02f9d782.png"
+  },
+  {
+    "revision": "d5d54c7d8fbe0671a77e9bf58a0507a1",
+    "url": "/static/media/zehnder.d5d54c7d.jpg"
+  },
+  {
+    "revision": "f6298cd4ab17fe05500ba94e19e55c7b",
+    "url": "/static/media/1.f6298cd4.png"
+  },
+  {
+    "revision": "fbf7d647b746726b236074602f7af697",
+    "url": "/static/media/2.fbf7d647.png"
+  },
+  {
+    "revision": "235746b8598de0508a93a43af700cf0f",
+    "url": "/static/media/3.235746b8.png"
+  },
+  {
+    "revision": "f210135219e590668b922170988c78ab",
+    "url": "/static/media/4.f2101352.png"
+  },
+  {
+    "revision": "f339835614d413cd08b9",
+    "url": "/static/css/1.06fb3819.chunk.css"
+  },
+  {
+    "revision": "d239bb07db9e7038345243e058f97526",
+    "url": "/index.html"
+  }
+];
